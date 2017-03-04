a6b9e1c163fbec6b849bf0eef358c8b76c8f1941
saileshkasaju <leosailesh@gmail.com>
2017-03-04 10:16:36 +0545

    populate dogs bu using each helper

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 (function() {
 
 	renderPage();
+	renderDogs();
 
 	function renderPage() {
 		var template = $('#index-template').html(),
@@ -10,5 +11,13 @@
 		$('#languageSwitch').click(function() {
 			DogPack.switchLanguage();
 		})
+	}
+
+	function renderDogs() {
+		var template = $('#dogs-template').html(),
+			compiled = Handlebars.compile(template),
+			rendered = compiled({dogs: DogPack.dogs, language: window.language});
+		$('#theDogs').html(rendered);
+
 	}
 })();
